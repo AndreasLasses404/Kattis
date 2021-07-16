3a74ad31a3064c76426fbe171e6c5bc63ff8e790
AndreasLasses404 <56150531+AndreasLasses404@users.noreply.github.com>
2021-07-16 14:01:26 +0200

    OddManOut

---
+++ Kattis/OddManOut.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+{
+    class OddManOut
+    {
+        public void OddMan()
+        {
+            int cases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cases; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                string[] guests = Console.ReadLine().Split();
+                Array.Sort(guests);
+                int oddGuest = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    string tempGuest = guests[j];
+                    int tempCount = 0;
+
+                    for (int k = 0; k < n; k++)
+                    {
+                        if (guests[k] == tempGuest)
+                        {
+                            tempCount++;
+                        }
+                    }
+                    if (tempCount == 1)
+                    {
+                        oddGuest = int.Parse(tempGuest);
+                    }
+                }
+
+                Console.WriteLine("Case #{0}: {1}", i + 1, oddGuest);
+
+            }
+        }
+    }
+}
+
